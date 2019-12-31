87cc3e1c84d3d9058358a2615d5eca4aaa1624ae
bellex0 <bellepx3@gmail.com>
2019-12-31 00:41:07 -0500

    home pg

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :locations
 
   # patch "/sessions/reset", to: "sessions#reset"
+  root 'sessions#new'
   get "/login", to: "sessions#new", as: "login"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
